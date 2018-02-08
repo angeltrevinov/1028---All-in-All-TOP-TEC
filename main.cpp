b143b26ee0b447a4206fc3344998ba71aff1edd4
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-02-08 10:14:58 -0600

    adding the initial files

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+  Angel Odiel Treviño Villanueva
+  A01336559
+  Problema: 1028 - All in All Fecha: 8/feb Materia: TopTec
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  /* Code Here */
+
+  return 0;
+}
