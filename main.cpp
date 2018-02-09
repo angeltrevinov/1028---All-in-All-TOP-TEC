7cf9c37f57a6f98062b08cf145b03052a2fdaf70
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-02-08 19:54:13 -0600

    solution

--- main.cpp
+++ main.cpp
@@ -5,42 +5,41 @@
 */
 
 #include <iostream>
-//#include <fstream>
+#include <vector>
 #include <string>
 
 using namespace std;
 
+bool Encryption(string normal, string encrypted){
+
+      int Posicion = 0;
+      for(int i = 0; i< normal.size(); i++){
+        Posicion = encrypted.find(normal[i], Posicion);
+        if(Posicion < 0){
+          return false;
+        }
+        Posicion++;
+      }
+      return true;
+}
+
 int main(){
     int Espacio;
-    string completo, normal, encrypted; //solo para leer el string y el encryptado
-    while(getline(cin, completo)){
-      Espacio = completo.find(" ");//encuentra la separacion de los dos strings
-      normal = completo.substr(0, Espacio);//remueve el primer strings
-      encrypted = completo.substr(Espacio);//remueve el ultimo string
-      if(normal.size() == encrypted.size()){//the strings size are equal thus they are not encrypted
-        cout << "NO" << endl;
+    string normal, encrypted; //solo para leer el string y el encryptado
+    vector<bool> Respuestas;
+    while((getline(cin,normal, ' ') && getline(cin, encrypted))){
+      if(Encryption(normal, encrypted)){
+         Respuestas.push_back(1);
       }else{
-        int pos = encrypted.find(normal);
-        if(pos >= 0 && normal == encrypted.substr(pos, normal.size())){//esta encadenado
-          cout << "YES" << endl;
-        }else{
-          int Arreglo[normal.size()];
-          bool ordenados = true;
-          for(int i = 0; i < normal.size(); i++){//para saber las posiciones del encryptado
-            Arreglo[i] = encrypted.find(normal[i]);
-          }
-          for(int i = 0; i < normal.size()-1; i++){//para checar si estan bien organizados
-            if(Arreglo[i] > Arreglo[i+1]){
-              ordenados = false;
-            }
-          }
-          if(!ordenados){
-            cout << "NO" << endl;
-          }else{
-            cout<< "YES" << endl;
-          }
-        }
-      }
+         Respuestas.push_back(0);
+       }
     }
+
+     for(int i = 0; i < Respuestas.size(); i++){
+         if(Respuestas[i])
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+     }
     return 0;
   }
